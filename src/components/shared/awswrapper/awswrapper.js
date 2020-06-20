dabecdb73f6c7f8d0546df8401e5e28623118fde
natesabrown <nateabrown@icloud.com>
2020-06-20 12:11:50 -0500

    Add smooth scrolling behavior and fix links to go to top of divs

--- src/components/shared/awswrapper/awswrapper.js
+++ src/components/shared/awswrapper/awswrapper.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './style.css'
 
 const AWSWrapper = ({ children, name, img, divid, isBlack }) => (
-    <div id={divid}>
+    <div>
         <div id="wrapperdiv" className={isBlack ? 'blackborder' : null}>
             <div id="titlediv">
+                <div id={divid}>
                 <img src={img} />
                 <span id="titletext">
                     {name}
                 </span>
+                </div>
             </div>
             <div id="childrendiv">
                 {children}
